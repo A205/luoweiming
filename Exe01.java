2619d77bdb5658fa801410025fd0343270799667
hjslsc <446901686@qq.com>
2015-07-07 17:39:19 +0800

    day01的作业
    第一天的作业

---
+++ Exe01.java
@@ -0,0 +1,16 @@
+package com.mathone.lwm;
+
+public class Exe01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		    boolean a1=false;
+	        boolean b1=true;
+	        System.out.println(a1+" "+b1);
+	        
+	}
+
+}
